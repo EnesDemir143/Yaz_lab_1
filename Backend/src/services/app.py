57040a91d2192cbafa8ee845af32e312af47fbb7
EnesDemir143 <enesd41@outlook.com>
2025-10-02 23:35:14 +0300

    Some file structer fixes and app is working now.

--- Backend/src/services/app.py
+++ Backend/src/services/app.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from Backend.src.services.Routes import department_coordinator
-from Routes import admin
+from Backend.src.services.Routes.department_coordinator import router as department_coordinator
+from Backend.src.services.Routes.admin import router as admin
+
 
 app = FastAPI()
 
-
-app.include_router(department_coordinator.router)
-app.include_router(admin.router)
+app.include_router(department_coordinator)
+app.include_router(admin)
