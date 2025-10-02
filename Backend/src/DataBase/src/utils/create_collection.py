ac2c57f6c694e229b1eb648ff7f4d9d8ddc6af4d
EnesDemir143 <enesd41@outlook.com>
2025-10-03 00:08:09 +0300

    collection create util, insert document util.

---
+++ Backend/src/DataBase/src/utils/create_collection.py
@@ -0,0 +1,9 @@
+from Backend.src.DataBase.src.utils.create_collection import get_database
+
+
+def create_collection(collection_name: str):
+    db = get_database()
+    if collection_name in db.list_collection_names():
+        return f"Collection '{collection_name}' already exists."
+    db.create_collection(collection_name)
+    return f"Collection '{collection_name}' created successfully."
---
+++ Backend/src/DataBase/src/utils/insert_document.py
@@ -0,0 +1,15 @@
+from Backend.src.DataBase.src.utils.create_collection import get_database
+from typing import Union, Dict, List
+
+
+def insert_document(collection_name: str, document: Union[Dict, List[Dict]]) -> str:
+    db = get_database()
+    collection = db[collection_name]
+
+    if isinstance(document, list):
+        result = collection.insert_many(document)
+        return ','.join(str(id) for id in result.inserted_ids)
+    else:
+        result = collection.insert_one(document)    
+
+    return str(result.inserted_id)
