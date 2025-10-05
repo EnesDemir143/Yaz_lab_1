b81bbe7eb85cb066896679a8771d3a6ebbfae4dd
EnesDemir143 <enesd41@outlook.com>
2025-10-05 20:15:16 +0300

    Class list menu fix.

--- Backend/src/DataBase/src/utils/class_list_menu.py
+++ Backend/src/DataBase/src/utils/class_list_menu.py
@@ -2,13 +2,9 @@ from Backend.src.DataBase.src.database_connection import get_database
 import pymysql
 
 def class_list_menu(department: str) -> list[dict]:
-    db = get_database()
-    cursor = db.cursor(pymysql.cursors.DictCursor)
-    
-    query = "SELECT * FROM classes WHERE department = %s"
-    cursor.execute(query, (department,))
-    classes = cursor.fetchall()
-    
-    cursor.close()
-    db.close()
-    return classes
+    with get_database() as db:
+        with db.cursor(pymysql.cursors.DictCursor) as cursor:
+            query = "SELECT * FROM classes WHERE department = %s"
+            cursor.execute(query, (department,))
+            return cursor.fetchall()
+            
