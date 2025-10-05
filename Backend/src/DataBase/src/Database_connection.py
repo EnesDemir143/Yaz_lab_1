591e9565ae5dcee82dd157838cf6363bed227a7e
EnesDemir143 <enesd41@outlook.com>
2025-10-05 20:02:01 +0300

    Db connection func fix.

--- Backend/src/DataBase/src/Database_connection.py
+++ Backend/src/DataBase/src/Database_connection.py
@@ -7,15 +7,15 @@ ENV_PATH = '../../../../.env'
 load_dotenv(ENV_PATH)
 
 def get_database() -> pymysql.connections.Connection:
-    mongo_uri = os.getenv("MONGO_URI")
-    if not mongo_uri:
-        raise ValueError("MONGO_URI is not set in the environment variables")
-    
-    connection = pymysql.connect(
-        host='mysql',
-        user=os.getenv("MYSQL_USER"),
-        password=os.getenv("MYSQL_PASSWORD"),
-        database=os.getenv("MYSQL_DATABASE"),
-        port=3306
-    )
+    try:
+        connection = pymysql.connect(
+            host='mysql',
+            user=os.getenv("MYSQL_USER"),
+            password=os.getenv("MYSQL_PASSWORD"),
+            database=os.getenv("MYSQL_DATABASE"),
+            port=3306
+        )
+    except pymysql.MySQLError as e:
+        print(f"Error connecting to database: {e}")
+        raise
     return connection
--- Backend/src/DataBase/src/utils/insert_document.py
+++
@@ -1,6 +0,0 @@
-from Backend.src.DataBase.src.database_connection import get_database
-from typing import Union, Dict, List
-
-
-def insert_document(collection_name: str, document: Union[Dict, List[Dict]]) -> str:
-    db = get_database()
